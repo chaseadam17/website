9fb5fe70e8bbfa14908fb5d9d6175bfc49f24c6c
Jared Cosulich <jared.cosulich@gmail.com>
2021-08-01 07:27:57 -0400

    working on page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,40 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="font-roboto-mono bg-gray-100">
       <Head>
         <title>Blank Foundation</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300,600&display=swap" rel="stylesheet" />
       </Head>
 
-      <main className="">
-        <h1 className="">
-          Welcome to Blank Foundation
-        </h1>
+      <div className="container mx-auto flex flex-col h-screen justify-between xl:px-36">
+        <header className="pt-12 flex flex-row justify-between">
+          <h1 className="font-semibold tracking-wide text-sm">
+            Blank
+            <span className="tracking-widest">.</span>
+            Foundation
+          </h1>
 
-      </main>
+          <div>
+            <a href="" className="pl-6 text-sm">Discord</a>
+            <a href="" className="pl-6 text-sm">Twitter</a>
+            <a href="" className="pl-6 text-sm">Medium</a>
+          </div>
 
-      <footer className="">
-        Footer
-      </footer>
+        </header>
+
+        <main className="">
+          <div className="bg-white" style={{width: "300px", height: "300px"}}></div>
+        </main>
+
+        <footer className="">
+          Footer
+        </footer>
+      </div>
+      
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto-mono': ['"Roboto Mono"']
+      }
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }
